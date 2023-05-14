d1b4df4f6c8a94140f44a599e5c2e5515a302025
Konrad Markowski <fortis211@gmail.com>
2023-05-14 22:07:41 +0200

    Added frontend for rounds

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,7 @@ class GameLogic {
         this.patternLen = patternLen;
         this.repeatColors = repeatColors;
         this.colorsArr = [];
+        this.round = 0;
     }
     createColorArr() {
         let offset = 50 * Math.random();
@@ -20,11 +21,70 @@ class GameLogic {
         this.colorsArr.forEach((element, index) => {
             let div = document.createElement("div");
             div.setAttribute("class", "colors");
-            div.setAttribute("id", 'color' + index);
+            div.setAttribute("id", "color" + index);
             div.style.backgroundColor = element;
             colorsSection.appendChild(div);
         });
     }
+    clear() {
+        const colorsSection = document.getElementById("selectColors");
+        colorsSection.innerHTML = "";
+        const gameArea = document.getElementById("gameArea");
+        gameArea.innerHTML = "";
+    }
+    createRound() {
+        const gameArea = document.getElementById("gameArea");
+        let gameRound = document.createElement("div");
+        gameRound.setAttribute("id", "round" + this.round);
+        gameRound.setAttribute("class", "gameRound");
+        let gameResult = document.createElement("div");
+        gameResult.setAttribute("id", "result" + this.round);
+        gameResult.setAttribute("class", "gameResult");
+        let gameGrid = document.createElement("div");
+        gameGrid.setAttribute("id", "grid" + this.round);
+        gameGrid.setAttribute("class", "gameGrid");
+        let gameCheck = document.createElement("div");
+        gameCheck.setAttribute("id", "check" + this.round);
+        gameCheck.setAttribute("class", "gameCheck");
+        for (let index = 0; index < this.patternLen; index++) {
+            let gameColor = document.createElement("div");
+            gameColor.style.backgroundColor = "white";
+            gameColor.setAttribute("class", "colors");
+            gameColor.setAttribute("id", "index" + index);
+            gameGrid.appendChild(gameColor);
+        }
+        let checkButton = document.createElement("button");
+        checkButton.setAttribute("id", 'button' + this.round);
+        checkButton.setAttribute("class", 'gameButton');
+        checkButton.innerText = 'Check';
+        checkButton.addEventListener("click", onCheckButton);
+        gameCheck.appendChild(checkButton);
+        let options = ['correct', 'misplaced', 'wrong'];
+        let colors = ['green', 'blue', 'red'];
+        for (let index = 0; index < 3; index++) {
+            let el = document.createElement('div');
+            el.setAttribute('id', options[index] + this.round);
+            el.setAttribute('class', "resultElement");
+            el.innerText = '0';
+            gameResult.appendChild(el);
+            let elIcon = document.createElement('div');
+            elIcon.style.backgroundColor = colors[index];
+            elIcon.setAttribute("class", "resultIcons");
+            gameResult.appendChild(elIcon);
+        }
+        gameRound.appendChild(gameResult);
+        gameRound.appendChild(gameGrid);
+        gameRound.appendChild(gameCheck);
+        gameArea.insertBefore(gameRound, gameArea.firstChild);
+        this.round += 1;
+    }
+}
+function onCheckButton(event) {
+    oGameLogic.createRound();
+    event.target.disabled = true;
+    let round = event.target.id;
+    round = round.slice(6);
+    document.getElementById('grid' + round).style.pointerEvents = 'none';
 }
 function numberInputLen(event) {
     let val = parseInt(event.target.value);
@@ -32,7 +92,7 @@ function numberInputLen(event) {
         document.getElementById("patternLengthInput").value =
             "10";
     }
-    if (val < 0) {
+    if (val <= 0) {
         document.getElementById("patternLengthInput").value =
             "1";
     }
@@ -43,7 +103,7 @@ function numberInputCol(event) {
         document.getElementById("numOfColorsInput").value =
             "25";
     }
-    if (val < 0) {
+    if (val <= 0) {
         document.getElementById("numOfColorsInput").value =
             "1";
     }
@@ -70,7 +130,9 @@ function onStartBtnClick(event) {
     let patternLen = parseInt(document.getElementById("patternLengthInput").value);
     let colorNum = parseInt(document.getElementById("numOfColorsInput").value);
     oGameLogic = new GameLogic(colorNum, patternLen, repeatColors);
+    oGameLogic.clear();
     oGameLogic.createColorArr();
     oGameLogic.displayColors();
+    oGameLogic.createRound();
     isInitialized = true;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 class GameLogic {
   public colorsArr: string[] = [];
+  public round: number = 0;
   constructor(
     public colorNumber: number,
     public patternLen: number,
@@ -23,11 +24,82 @@ class GameLogic {
     this.colorsArr.forEach((element, index) => {
       let div = document.createElement("div");
       div.setAttribute("class", "colors");
-      div.setAttribute("id",'color'+index);
+      div.setAttribute("id", "color" + index);
       div.style.backgroundColor = element;
       colorsSection.appendChild(div);
     });
   }
+  clear() {
+    const colorsSection = document.getElementById(
+      "selectColors"
+    ) as HTMLDivElement;
+    colorsSection.innerHTML = "";
+    const gameArea = document.getElementById("gameArea") as HTMLDivElement;
+    gameArea.innerHTML = "";
+  }
+  createRound() {
+    const gameArea = document.getElementById("gameArea") as HTMLDivElement;
+    let gameRound = document.createElement("div");
+    gameRound.setAttribute("id", "round" + this.round);
+    gameRound.setAttribute("class","gameRound");
+
+    let gameResult = document.createElement("div");
+    gameResult.setAttribute("id", "result" + this.round);
+    gameResult.setAttribute("class","gameResult");
+  
+    let gameGrid = document.createElement("div");
+    gameGrid.setAttribute("id", "grid" + this.round);
+    gameGrid.setAttribute("class","gameGrid");
+
+    let gameCheck = document.createElement("div");
+    gameCheck.setAttribute("id", "check" + this.round);
+    gameCheck.setAttribute("class","gameCheck");
+
+    for (let index = 0; index < this.patternLen; index++) {
+      let gameColor = document.createElement("div");
+      gameColor.style.backgroundColor = "white";
+      gameColor.setAttribute("class", "colors");
+      gameColor.setAttribute("id", "index" + index);
+      gameGrid.appendChild(gameColor);
+    }
+
+    let checkButton = document.createElement("button");
+    checkButton.setAttribute("id",'button'+this.round);
+    checkButton.setAttribute("class",'gameButton');
+    checkButton.innerText='Check';
+    checkButton.addEventListener("click", onCheckButton);
+    gameCheck.appendChild(checkButton);
+    
+    let options = ['correct', 'misplaced','wrong'];
+    let colors = ['green','blue','red'];
+    for (let index = 0; index < 3; index++) {
+      let el = document.createElement('div');
+      el.setAttribute('id',options[index]+this.round);
+      el.setAttribute('class',"resultElement");
+      el.innerText='0';
+      gameResult.appendChild(el);
+
+      let elIcon = document.createElement('div');
+      elIcon.style.backgroundColor = colors[index];
+      elIcon.setAttribute("class", "resultIcons");
+      gameResult.appendChild(elIcon);
+    }
+
+    gameRound.appendChild(gameResult);
+    gameRound.appendChild(gameGrid);
+    gameRound.appendChild(gameCheck);
+
+    gameArea.insertBefore(gameRound, gameArea.firstChild);
+    this.round+=1;
+  }
+}
+
+function onCheckButton(event:Event){
+  oGameLogic.createRound();
+  (event.target as HTMLButtonElement).disabled = true;
+  let round=(event.target as HTMLButtonElement).id;
+  round = round.slice(6);
+  (document.getElementById('grid'+round) as HTMLDivElement).style.pointerEvents='none';
 }
 
 function numberInputLen(event: Event) {
@@ -36,7 +108,7 @@ function numberInputLen(event: Event) {
     (document.getElementById("patternLengthInput") as HTMLInputElement).value =
       "10";
   }
-  if (val < 0) {
+  if (val <= 0) {
     (document.getElementById("patternLengthInput") as HTMLInputElement).value =
       "1";
   }
@@ -47,7 +119,7 @@ function numberInputCol(event: Event) {
     (document.getElementById("numOfColorsInput") as HTMLInputElement).value =
       "25";
   }
-  if (val < 0) {
+  if (val <= 0) {
     (document.getElementById("numOfColorsInput") as HTMLInputElement).value =
       "1";
   }
@@ -84,7 +156,9 @@ function onStartBtnClick(event: Event) {
     (document.getElementById("numOfColorsInput") as HTMLInputElement).value
   );
   oGameLogic = new GameLogic(colorNum, patternLen, repeatColors);
+  oGameLogic.clear();
   oGameLogic.createColorArr();
   oGameLogic.displayColors();
+  oGameLogic.createRound();
   isInitialized = true;
 }
